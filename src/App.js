bebab29501526536e50b5416ccd093a2582a9f7f
esteban90-dev <stephencallard@gmail.com>
2022-07-13 17:00:41 -0600

    setup routes to display the photos page and the cart page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import {Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
+import Photos from './pages/Photos';
+import Cart from './pages/Cart';
 
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Photos />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+    </>
   )
 }
