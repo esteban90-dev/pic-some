1adadc37f4b4899f61ec400ea0993279cc79acf3
esteban90-dev <stephencallard@gmail.com>
2022-07-13 16:41:00 -0600

    add the header component

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1>Pic Some</h1>
+      <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+    </header>
+  )
+}
