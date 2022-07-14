8d8a6ce6a19a550baf580f61feb5f87f88d98c45
esteban90-dev <stephencallard@gmail.com>
2022-07-14 07:58:22 -0600

    fetch photos and store in context

--- src/context.js
+++ src/context.js
@@ -1,9 +1,17 @@
-import {createContext, useState} from 'react';
+import {createContext, useEffect, useState} from 'react';
 
 const {Provider, Consumer} = createContext();
 
 function ContextProvider(props) {
   const [allPhotos, setAllPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')
+      .then(data => data.json())
+      .then(json => 
+        setAllPhotos(json)
+      );
+  }, []);
 
   return (
     <Provider value={{allPhotos}}>
