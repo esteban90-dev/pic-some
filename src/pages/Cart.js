ba635ce1fded7b66515c07c83c14d77b720dc224
esteban90-dev <stephencallard@gmail.com>
2022-07-14 16:26:37 -0600

    display total cost of cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -9,11 +9,17 @@ export default function Cart() {
     return <CartItem key={cartItem.id} item={cartItem} />
   })
 
+  function totalCost() {
+    // we are using $5.99 as a fake cost,
+    // so just multiply that times number of cart items
+    return cartItems.length * 5.99;
+  }
+
   return (
     <main className="cart__page">
       <h1 className="cart__title">Check Out</h1>
      {cartItemElements}
-     <h2 className="cart__total">Total:</h2>
+     <h2 className="cart__total">Total: ${totalCost()}</h2>
      <div className="cart__buttonContainer">
       <button className="cart__orderButton">Place Order</button>
      </div>
