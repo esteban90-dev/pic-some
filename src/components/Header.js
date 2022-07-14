09591ed76899594be122c86442c03ea6f52b6e90
esteban90-dev <stephencallard@gmail.com>
2022-07-14 15:36:05 -0600

    display full cart when cart has items in it

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Context} from '../context';
 
 export default function Header() {
+  const {cartItems} = React.useContext(Context);
+
+  const isCartFull = cartItems.length > 0;
+
   return (
     <header className="header">
       <Link to="/">
         <h1>Pic Some</h1>
       </Link>
       <Link to="/cart">
+        {isCartFull ? 
+        <i className="ri-shopping-cart-fill ri-fw ri-2x"></i> :
         <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        }
+        
       </Link>
     </header>
   )
