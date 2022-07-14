6f07346e3ce6f812787d08a175d3dcfb94d922ce
esteban90-dev <stephencallard@gmail.com>
2022-07-14 16:18:37 -0600

    clicking the trash icon remove the item from the cart

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import {Context} from '../context';
 
 export default function CartItem(props) {
   const {item} = props;
+
+  const {removeFromCart} = React.useContext(Context);
 
   return (
     <section className="cartItem">
       <div className="cartItem__imageContainer">
         <img src={item.url} className="cartItem__image" alt=""></img>
+        <i className="ri-delete-bin-line" onClick={() => removeFromCart(item)}></i>
       </div>
       <h2>$5.99</h2>
     </section>
