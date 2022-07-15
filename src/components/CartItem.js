4829b47d54f614d614c7ff386ba618679a42d6a7
esteban90-dev <stephencallard@gmail.com>
2022-07-15 07:55:28 -0600

    display trash icon filled when mouse hovers over it

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,14 +3,31 @@ import {Context} from '../context';
 
 export default function CartItem(props) {
   const {item} = props;
+  const [isHovered, setIsHovered] = React.useState(false);
 
   const {removeFromCart} = React.useContext(Context);
+
+  const trashIconClass = isHovered ? "ri-delete-bin-fill" : "ri-delete-bin-line";
+
+  function handleMouseEnter() {
+    setIsHovered(true);
+  }
+
+  function handleMouseLeave() {
+    setIsHovered(false);
+  }
 
   return (
     <section className="cartItem">
       <div className="cartItem__imageContainer">
         <img src={item.url} className="cartItem__image" alt=""></img>
-        <i className="ri-delete-bin-line" onClick={() => removeFromCart(item)}></i>
+        <i  
+          className={trashIconClass}
+          onClick={() => removeFromCart(item)}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+        </i>
       </div>
       <h2>$5.99</h2>
     </section>
